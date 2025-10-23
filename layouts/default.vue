d9b38d1400d7c2229e1ee30a5b6c8a5fa6ec17d5
kbcAvi <160338862+kbcAvi@users.noreply.github.com>
2025-10-23 15:48:32 +0530

    changes

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+    <NuxtLayout>
+      <v-app>
+        <v-main>
+            <NuxtPage />
+        </v-main>
+      </v-app>
+    </NuxtLayout>
+  </template>
+  
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur laboriosam asperiores corporis veritatis minus corrupti atque cum magni iusto porro. Est quos exercitationem illum harum blanditiis id ipsam voluptates enim, vitae cupiditate dicta. Voluptate odit corrupti perspiciatis fugiat dolore possimus maxime omnis, fuga facere quo nemo corporis iure! Minus laudantium dolores maiores ipsa reprehenderit quas corrupti fugiat officia voluptates, vel assumenda consequuntur tempore cumque at similique culpa recusandae libero odio dignissimos optio quisquam earum sunt praesentium aspernatur. Ipsum asperiores illo dignissimos eos? Alias facere dolor temporibus minus pariatur. Ab nulla eaque reiciendis quo id veritatis culpa repudiandae deserunt blanditiis explicabo.
+    </div>
+</template>
+<script setup>
+</script>
