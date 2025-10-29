3a679c5c90e93718656a00cfba06f3cc1bbc6a3e
kbcAvi <160338862+kbcAvi@users.noreply.github.com>
2025-10-29 13:00:05 +0530

    setting changes

--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,4 +1,5 @@
 <template>
+
   <v-container fluid>
     <h3>Settings
     </h3>
@@ -31,12 +32,66 @@
                 placeholder="Modern 3BR Apartment"></v-text-field>
             </div>
           </v-col>
-          <v-col cols="12">
+          <v-col cols="12" md="4">
+            <div>
+              <p>Gender</p>
+              <v-select :items="['Male', 'Female']" :error-messages="v$.GENDER.$errors.map(e => e.$message)"
+                @blur="v$.GENDER.$touch" @input="v$.GENDER.$touch" v-model="state.GENDER" class="mt-1" rounded="lg"
+                variant="outlined" placeholder="Modern 3BR Apartment"></v-select>
+            </div>
+          </v-col>
+          <v-col cols="12" md="4">
+            <div>
+              <p>State</p>
+              <v-text-field :error-messages="v$.STATE.$errors.map(e => e.$message)" @blur="v$.STATE.$touch"
+                @input="v$.STATE.$touch" v-model="state.STATE" class="mt-1" rounded="lg" variant="outlined"
+                placeholder="State"></v-text-field>
+            </div>
+          </v-col>
+          <v-col cols="12" md="4">
+            <div>
+              <p>City</p>
+              <v-text-field :error-messages="v$.CITY.$errors.map(e => e.$message)" @blur="v$.CITY.$touch"
+                @input="v$.CITY.$touch" v-model="state.CITY" class="mt-1" rounded="lg" variant="outlined"
+                placeholder="City"></v-text-field>
+            </div>
+          </v-col>
+
+          <v-col cols="12" md="6">
             <div>
               <p>Email</p>
               <v-text-field :error-messages="v$.EMAIL.$errors.map(e => e.$message)" @blur="v$.EMAIL.$touch"
                 @input="v$.EMAIL.$touch" v-model="state.EMAIL" class="mt-1" rounded="lg" variant="outlined"
-                placeholder="jhon@gmail.com"></v-text-field>
+                placeholder="jhon@gmail.com">
+
+                <template v-slot:prepend-inner>
+                  <v-icon :color="authStore.getUserDetails.IS_EMAIL_VERIFIED_FLG == 1 ? 'success' : 'info'">{{authStore.getUserDetails.IS_EMAIL_VERIFIED_FLG == 1 ? 'mdi-check-circle' : 'mdi-alert-circle-outline'}}</v-icon>
+                </template>
+              </v-text-field>
+            </div>
+          </v-col>
+          <v-col cols="12" md="6">
+            <div>
+              <p>Mobile Number</p>
+              <v-text-field readonly type="number" :error-messages="v$.MOBILE_PHONE.$errors.map(e => e.$message)"
+                @blur="v$.MOBILE_PHONE.$touch" @input="v$.MOBILE_PHONE.$touch" v-model="state.MOBILE_PHONE" class="mt-1"
+                rounded="lg" variant="outlined" placeholder="Mobile number">
+              
+                <template v-slot:prepend-inner>
+                  <v-icon :color="authStore.getUserDetails.IS_MOBILE_VERIFIED_FLG == 1 ? 'success' : ''">mdi-check-circle</v-icon>
+                </template>
+              </v-text-field>
+              <!-- <v-number-input variant="outlined" :error-messages="v$.MOBILE_PHONE.$errors.map(e => e.$message)" @blur="v$.MOBILE_PHONE.$touch"
+                  @input="v$.MOBILE_PHONE.$touch" v-model="state.MOBILE_PHONE" control-variant="hidden"></v-number-input> -->
+            </div>
+
+          </v-col>
+          <v-col cols="12">
+            <div>
+              <p>Address</p>
+              <v-textarea :error-messages="v$.ADDRESS.$errors.map(e => e.$message)" @blur="v$.ADDRESS.$touch"
+                @input="v$.ADDRESS.$touch" v-model="state.ADDRESS" class="mt-1" rounded="lg" variant="outlined"
+                placeholder="Address"></v-textarea>
             </div>
           </v-col>
         </v-row>
@@ -60,10 +115,12 @@ import propertyService from '@/services/propertyService.js'
 import { useVuelidate } from '@vuelidate/core'
 import { required, helpers, minLength, maxLength, numeric, email } from '@vuelidate/validators'
 import { toast } from 'vue3-toastify';
+import { useAuthStore } from '@/stores/app';
 
+const authStore = useAuthStore()
 const state = ref({
   ACTION_TYPE: "UPDATE",
-  USER_ID: 0,
+  USER_ID: authStore.getUserDetails.USER_ID,
   MOBILE_PHONE: "",
   FNAME: "",
   MNAME: "",
@@ -83,10 +140,14 @@ const state = ref({
 
 const rules = {
   FNAME: { required: helpers.withMessage('First Name is required', required) },
-  MNAME: { required: helpers.withMessage('Middle Name is required', required) },
+  MNAME: { },
   LNAME: { required: helpers.withMessage('Last Name is required', required) },
   EMAIL: { required: helpers.withMessage('Email is required', required) },
-
+  GENDER: {},
+  MOBILE_PHONE: { required: helpers.withMessage('Phone Number is required', required, numeric) },
+  ADDRESS: {},
+  STATE: {},
+  CITY: {}
 }
 const v$ = useVuelidate(rules, state.value)
 
@@ -120,6 +181,17 @@ const updateProfile = async () => {
     }
   }
 }
-
+onMounted(() => {
+  state.value.FNAME = authStore.getUserDetails.FNAME
+  state.value.MNAME = authStore.getUserDetails.MNAME
+  state.value.LNAME = authStore.getUserDetails.LNAME
+  state.value.EMAIL = authStore.getUserDetails.EMAIL
+  state.value.GENDER = authStore.getUserDetails.GENDER
+  state.value.MOBILE_PHONE = authStore.getUserDetails.MOBILE_PHONE
+  state.value.ADDRESS = authStore.getUserDetails.ADDRESS
+  state.value.CITY = authStore.getUserDetails.CITY
+  state.value.STATE = authStore.getUserDetails.STATE
+  state.value.USER_ID = authStore.getUserDetails.USER_ID
+})
 
 </script>
