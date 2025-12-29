6dac545daaffd807a5cecc9c9be3e248729a13ef
jabir-kbc <kbcincjabir@gmail.com>
2025-12-29 16:15:59 +0530

    new updates

--- src/components/AboutPage.vue
+++ src/components/AboutPage.vue
@@ -1,9 +1,23 @@
 <template>
+  <v-app-bar density="comfortable" flat class="pa-2 border-b">
+    <template v-slot:prepend>
+      <div class="d-flex align-center">
+        <!-- <v-img width="70" contain src="@/assets/newLogo.png"></v-img> -->
+        <v-btn @click="$router.push({ name: 'Login' })" size="small" variant="tonal" min-width="100" color="primary" class="text-none box-shadow ml-4 text-body-2 mt-1"> Login </v-btn>
+      </div>
+    </template>
+    <v-btn class="text-none rounded-lg font-weight-bold card-box-shadow" variant="tonal" color="primary" min-width="150" @click="$router.push('/signup')"> Sign Up For Free</v-btn>
+  </v-app-bar>
+
   <section class="text-center pa-10 py-16 d-flex flex-column justify-center align-center" style="background: linear-gradient(135deg, #EFF6FF 0%, #DBEAFE 50%, #BFDBFE 100%);">
     <p class="bg-white px-4 py-2 rounded-pill text-primary font-weight-bold mb-16 card-box-shadow mt-10" style="max-width: fit-content;">🚀 Revolutionizing Real Estate Transactions</p>
-    <h3 class="text-primary text-h1 font-weight-bold mb-10">Skip the Middleman. <br> Connect Directly.</h3>
+    <v-row>
+      <v-col lg="text-h1" md="text-h2">
+        <h3 class="text-primary text-h2 font-weight-bold mb-10">Skip the Middleman. <br> Connect Directly.</h3>
+      </v-col>
+    </v-row>
     <p class="text-h5 text-grey-darken-1 mb-10">Buy, Sell, or Rent Properties Without Intermediaries Using Smart QR <br> Code Technology</p>
-    <div class="d-flex ga-8 mb-10">
+    <div class="d-flex ga-8 mb-10 flex-wrap justify-center">
       <v-btn @click="$router.push({ name: 'Login' })" class="text-none font-weight-bold rounded-lg card-box-shadow btn-hover" size="x-large" color="#F97316">Get Started for Free</v-btn>
       <v-btn @click="$router.push({ name: 'Login' })" class="text-none font-weight-bold rounded-lg card-box-shadow" size="x-large" variant="outlined" append-icon="mdi-play">Watch How it Works</v-btn>
     </div>
@@ -26,35 +40,40 @@
   <section class="text-center pa-10 py-16 d-flex flex-column justify-center align-center">
     <h3 class="text-h2 font-weight-bold mb-16 mt-16">Why Traditional Real Estate is Broken</h3>
     <div class="d-flex ga-10 mb-16 flex-wrap justify-center">
-      <v-card class="pa-14 rounded-xl elevation-0 card-hover" width="400" style="background: linear-gradient(135deg, #FEF2F2 0%, #FEE2E2 100%);">
+      <!-- <v-card class="pa-14 rounded-xl elevation-0 card-hover" width="400" style="background: linear-gradient(135deg, #FEF2F2 0%, #FEE2E2 100%);"> -->
+      <v-card class="pa-14 rounded-xl elevation-0 card-hover" width="400" style="background: linear-gradient(135deg, rgb(254 249 242) 0%, rgb(254 234 226) 100%);">
         <h3 class="text-h2 mb-6">💸</h3>
-        <h4 class="text-h5 font-weight-bold mb-6 " style="color: #DC2626;">High Commission</h4>
+        <!-- DC2626 -->
+        <h4 class="text-h5 font-weight-bold mb-6 " style="color: #F97316;">High Commission</h4>
         <p class="text-grey-darken-1">2-5% in fees eating into your profits</p>
       </v-card>
-      <v-card class="pa-14 rounded-xl elevation-0 card-hover" width="400" style="background: linear-gradient(135deg, #FEF2F2 0%, #FEE2E2 100%);">
+      <v-card class="pa-14 rounded-xl elevation-0 card-hover" width="400" style="background: linear-gradient(135deg, rgb(254 249 242) 0%, rgb(254 234 226) 100%);">
         <h3 class="text-h2 mb-6">⏰</h3>
-        <h4 class="text-h5 font-weight-bold mb-6" style="color: #DC2626;">Lengthy Process</h4>
+        <h4 class="text-h5 font-weight-bold mb-6" style="color: #F97316;">Lengthy Process</h4>
         <p class="text-grey-darken-1">Weeks or months to close deals</p>
       </v-card>
-      <v-card class="pa-14 rounded-xl elevation-0 card-hover" width="400" style="background: linear-gradient(135deg, #FEF2F2 0%, #FEE2E2 100%);">
+      <v-card class="pa-14 rounded-xl elevation-0 card-hover" width="400" style="background: linear-gradient(135deg, rgb(254 249 242) 0%, rgb(254 234 226) 100%);">
         <h3 class="text-h2 mb-6">🔗</h3>
-        <h4 class="text-h5 font-weight-bold mb-6" style="color: #DC2626;">Too Many Intermediaries</h4>
+        <h4 class="text-h5 font-weight-bold mb-6" style="color: #F97316;">Too Many Intermediaries</h4>
         <p class="text-grey-darken-1">Multiple middlemen slowing everything down</p>
       </v-card>
     </div>
   </section>
 
   <section class="text-center pa-10 py-16 d-flex justify-center align-center" style="background-color: #F9FAFB;">
-    <v-row justify="center" align="center" class="px-16 ">
-      <v-col class="text-left my-10" style="max-width: 760px;">
+    <v-row justify="center" align="center" class="">
+      <v-col class="text-left my-10" style="max-width: 760px;" md="px-6" lg="px-16">
         <h3 class="text-h2 font-weight-bold mb-10" style="background: linear-gradient(135deg, #2563eb, #10b98f); background-clip: text;-webkit-text-fill-color: transparent; line-height: 1.6;">The QRForInfo <br> Solution</h3>
         <p class="font-italic text-h5 text-grey-darken-1 mb-10">Direct. Simple. Transparent.</p>
         <p class="text-h5 mb-10">Connect buyers and sellers directly through smart QR codes. No agents, no commissions, no delays. Just instant property sharing and direct communication.</p>
         <v-btn @click="$router.push({ name: 'Login' })" class="btn-hover text-none font-weight-bold rounded-lg card-box-shadow" size="x-large" color="#F97316" append-icon="mdi-arrow-right-thin">Learn More</v-btn>
       </v-col>
 
-      <v-col cols="auto" class="">
-        <v-card class="bg-white pa-10 card-box-shadow rounded-xl text-center" width="520">
+      <v-col cols="auto" class="" md="px-6" lg="px-16">
+        <v-card v-if="false" class="bg-white pa-10 card-box-shadow rounded-xl text-center" width="520">
+          <v-img class="w-100 h-100" src="@/assets/qr-banner.jpeg"></v-img>
+        </v-card>
+        <v-card class="bg-white pa-10 card-box-shadow rounded-xl text-center" width="520" style="max-width: 70vw;">
           <v-card class="pa-10 mb-6 rounded-xl d-flex justify-center align-center" height="200" style="background: linear-gradient(135deg, #2563eb, #1e40af);">
             <v-card class="pa-4 rounded-lg bg-white" width="100" height="100">
               <!-- <v-icon size="x-large">mdi-qrcode-scan</v-icon> -->
@@ -72,7 +91,7 @@
   <section class="text-center pa-10 py-16 d-flex flex-column justify-center align-center">
     <h3 class="text-h2 font-weight-bold mb-16 mt-16">How It Works</h3>
     <div class="text-left mb-10">
-      <h5 class="text-primary font-weight-black mb-10 pb-3" style="border-bottom: 3px solid #2563eb;max-width: fit-content; font-size: 2.2rem !important;">FOR SELLERS</h5>
+      <h5 class="text-primary font-weight-black mb-10 pb-4" style="border-bottom: 4px solid #2563eb;max-width: fit-content; font-size: 2.2rem !important; font-family: 'Sora', sans-serif;">FOR SELLERS</h5>
 
       <div class="d-flex ga-10 mb-16 flex-wrap justify-center">
         <v-card class="card-hover card-blue-hover pa-10 rounded-xl elevation-0 overflow-visible" color="#F9FAFB" width="350">
@@ -104,7 +123,7 @@
     </div>
 
     <div class="text-left">
-      <h5 class="text-primary font-weight-black mb-10 pb-3" style="border-bottom: 3px solid #2563eb;max-width: fit-content; font-size: 2.2rem !important;">FOR BUYERS</h5>
+      <h5 class="text-primary font-weight-black mb-10 pb-4" style="border-bottom: 4px solid #2563eb;max-width: fit-content; font-size: 2.2rem !important; font-family: 'Sora', sans-serif;">FOR BUYERS</h5>
 
       <div class="d-flex ga-10 mb-16 flex-wrap justify-center">
         <v-card class="card-hover card-blue-hover pa-10 rounded-xl elevation-0 overflow-visible" color="#F9FAFB" width="350">
@@ -221,26 +240,26 @@
     <h3 class="text-h2 font-weight-bold mb-10 mt-16">Ready to Revolutionize Your Property Transaction?</h3>
     <p class="text-h5 mb-16">Join Thousands of Smart Property Owners and Buyers Today</p>
     <div class="d-flex ga-10 mb-16 flex-wrap justify-center text-left">
-      <v-card class="card-bottom-hover pa-10 rounded-xl elevation-0 bg-white card-box-shadow text-center" width="400">
+      <v-card class="card-bottom-hover pa-10 rounded-xl elevation-0 bg-white card-box-shadow text-center" width="400" style="max-width: 70vw;">
         <h4 class="text-h4 font-weight-bold mb-10 text-primary">I Want to Sell</h4>
         <v-btn @click="$router.push({ name: 'Login' })" class="btn-hover text-none font-weight-bold rounded-lg card-box-shadow" append-icon="mdi-arrow-right-thin" size="x-large" color="#F97316">
           Get Started
         </v-btn>
       </v-card>
-      <v-card class="card-bottom-hover pa-10 rounded-xl elevation-0 bg-white card-box-shadow text-center" width="400">
+      <v-card class="card-bottom-hover pa-10 rounded-xl elevation-0 bg-white card-box-shadow text-center" width="400" style="max-width: 70vw;">
         <h4 class="text-h4 font-weight-bold mb-10 text-primary">I Want to Buy</h4>
         <v-btn @click="$router.push({ name: 'BuyerLanding' })" class="btn-hover text-none font-weight-bold rounded-lg card-box-shadow" append-icon="mdi-arrow-right-thin" size="x-large" color="#F97316">
           Browse Listing
         </v-btn>
       </v-card>
     </div>
-    <p class="text-h6 font-weight-regular mb-16">✓ Free to get started <span class="ms-8">✓ No credit card required</span></p>
+    <p class="text-h6 font-weight-regular mb-16 d-flex flex-wrap justify-center text-center ga-6">✓ Free to get started <span class="">✓ No credit card required</span></p>
   </section>
 
   <section class="text-center pa-10 py-16" style="background-color: #1F2937;">
     <div class="d-flex justify-center align-start text-white flex-wrap pb-10" style="gap: 100px 100px;">
       <div class="text-left">
-        <h3 class="text-h3 font-weight-bold mb-10 text-primary">QRForInfo</h3>
+        <h3 class="text-h3 font-weight-bold mb-10 text-white">QRForInfo</h3>
         <p class="text-h6">Connecting Property Buyers and Sellers Directly</p>
       </div>
       <div class="">
@@ -281,7 +300,7 @@
       </div>
     </div>
     <v-divider class="text-white mt-16"></v-divider>
-    <p class="text-white pt-10 text-h6 text-grey-lighten-1">© 2026 QRForInfo. All rights reserved.</p>
+    <p class="text-white pt-10 text-h6 text-grey-lighten-1">© {{ new Date().getFullYear() }} QRForInfo. All rights reserved.</p>
   </section>
 </template>
 
