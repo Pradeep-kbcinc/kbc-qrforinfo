528eee8ad41b5c25669c183c8a5338c49737d450
kbcAvi <160338862+kbcAvi@users.noreply.github.com>
2026-01-02 22:27:19 +0530

    view count added

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -116,13 +116,14 @@
                 <v-list-item-title class="font-weight-bold text-grey-darken-3">{{ propertyObj.TITLE
                   }}</v-list-item-title>
                 <!-- <v-list-item-subtitle>{{ property.location }}</v-list-item-subtitle> -->
-                <v-list-item-subtitle v-if="(propertyObj.IS_ADDRESS_PRIVATE_FLG == 1) && propertyObj.ADDRESS_LINE1" class="mt-1"><v-icon color="black">mdi-map-marker</v-icon>{{ propertyObj.ADDRESS_LINE1 }}</v-list-item-subtitle>
+                <v-list-item-subtitle v-if="(propertyObj.IS_ADDRESS_PRIVATE_FLG == 1) && propertyObj.ADDRESS_LINE1" class="mt-1"><v-icon color="primary">mdi-map-marker</v-icon>{{ propertyObj.ADDRESS_LINE1 }}</v-list-item-subtitle>
+                <v-list-item-subtitle class="mt-1"><v-icon class="mr-2" color="green-darken-1">mdi-eye</v-icon><strong class="text-subtitle-2 text-black font-weight-bold">{{ propertyObj.VIEW_COUNT }}</strong></v-list-item-subtitle>
                  <p class="mt-2">Created Date : {{ moment(propertyObj.CREATED_ON).format('Do MMM, YYYY') }}</p> 
                 <div class="text-primary font-weight-bold text-subtitle-1">{{ propertyObj.price }}</div>
 
                 <template #append>
                   <div class="text-end">
-
+                    
                     <v-btn @click.stop="openModal(propertyObj)" rounded="lg" class="text-none font-weight-bold text-subtitle-1" min-width="100" height="48" color="primary" elevation="0">Add Images</v-btn>
                   </div>
                 </template>
